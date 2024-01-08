96e819057c810dd29dd05777222d711149b791b6
Md. Razibul Hasan Mithu <razibul.mithu@shikho.com>
2024-01-08 13:56:33 +0600

    regen functionality update

--- generator/generator.go
+++ generator/generator.go
@@ -7,36 +7,39 @@ import (
 
 // Run generates code based on the parsed YAML data.
 func Run(data *Data) {
-	if file.IsFileExists(filepath.Join("domain", data.FileName)) {
-		if askForConfirmation(filepath.Join("domain", data.FileName) + " file already exist\nWant to regenerate it?") {
-			if err := file.RemoveFile(filepath.Join("domain", data.FileName)); err != nil {
-				panic(err)
-			}
-			generateDomainModule(data)
-		}
-	} else {
-		generateDomainModule(data)
+	modules := map[string]struct {
+		Trigger    func(*Data)
+		ModulePath string
+	}{
+		"domain": {
+			Trigger:    generateDomainModule,
+			ModulePath: filepath.Join("domain", data.FileName),
+		},
+		"repository": {
+			Trigger:    generateInterface,
+			ModulePath: filepath.Join("domain", "repository", data.FileName),
+		},
+		"infrastructure": {
+			Trigger:    generateInfraModule,
+			ModulePath: filepath.Join("infrastructure", "repository", "schema", data.FileName),
+		},
 	}
 
-	if file.IsFileExists(filepath.Join("domain", "repository", data.FileName)) {
-		if askForConfirmation(filepath.Join("domain", "repository", data.FileName) + " file already exist\nWant to regenerate it?") {
-			if err := file.RemoveFile(filepath.Join("domain", "repository", data.FileName)); err != nil {
-				panic(err)
+	// Regenerate modules based on file existence
+	regenerate := func(moduleType string) {
+		module := modules[moduleType]
+		if file.IsFileExists(module.ModulePath) {
+			if askForConfirmation(module.ModulePath + " file already exists. Do you want to regenerate it?") {
+				if err := file.RemoveFile(module.ModulePath); err != nil {
+					panic(err)
+				}
+				module.Trigger(data)
 			}
-			generateInterface(data)
+		} else {
+			module.Trigger(data)
 		}
-	} else {
-		generateInterface(data)
 	}
-
-	if file.IsFileExists(filepath.Join("infrastructure", "repository", "schema", data.FileName)) {
-		if askForConfirmation(filepath.Join("infrastructure", "repository", "schema", data.FileName) + " file already exist\nWant to regenerate it?") {
-			if err := file.RemoveFile(filepath.Join("infrastructure", "repository", "schema", data.FileName)); err != nil {
-				panic(err)
-			}
-			generateInfraModule(data)
-		}
-	} else {
-		generateInfraModule(data)
+	for _, moduleType := range []string{"domain", "repository", "infrastructure"} {
+		regenerate(moduleType)
 	}
 }
